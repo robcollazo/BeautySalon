d95bc5b8ce234bc2d91aaf9d7644ac260fdd2507
Roberto Collazo <58235825+robcollazo@users.noreply.github.com>
2021-07-02 20:35:17 -0300

    Adding more sections
    - Added testimonials and contact sections
    - Added ScrollReveal and Swiper

--- main.js
+++ main.js
@@ -30,3 +30,33 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+// Testimonials Carousel
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// Scroll Reveal
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .text, #home .image,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links
+`,
+  { interval: 100 }
+)
