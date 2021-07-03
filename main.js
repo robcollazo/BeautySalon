651ccf9c8163dcd2c4a60ca371478a5fffa930ee
Roberto Collazo <58235825+robcollazo@users.noreply.github.com>
2021-07-03 00:55:33 -0300

    Footer
    Added Footer;
    Added a go to top button;

--- main.js
+++ main.js
@@ -20,16 +20,17 @@ for (const link of links) {
 }
 
 // Adiciona sombra no header quando scroll
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 // Testimonials Carousel
 
@@ -56,7 +57,24 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+// Back to top Button
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
